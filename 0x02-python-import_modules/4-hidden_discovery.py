3711271473481b45bea291efc864e4dea16d8adf
Santiago Agudelo <sagudelo1200@hmail.com>
2020-04-30 23:15:44 -0500

    Task 4 | Test 0

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    li = dir(hidden_4)
+    for i in range(0, len(li)):
+        if(li[i][0] != "_" and li[i][1] != "_"):
+            print("{:s}".format(li[i]))
+            
