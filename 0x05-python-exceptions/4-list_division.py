ec335e7886aad9277a16ce472d61aa450fe2a0e8
Santiago Agudelo <sagudelo1200@gmail.com>
2020-05-18 21:47:28 -0500

    position for change value of result was deplicated in the excepts

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,16 +2,18 @@
 def list_division(my_list_1, my_list_2, list_length):
     length = []
     count = 0
-    result = 0
     while count < list_length:
         try:
             result = my_list_1[count] / my_list_2[count]
         except ZeroDivisionError:
             print("division by 0")
+            result = 0
         except TypeError:
             print("wrong type")
+            result = 0
         except IndexError:
             print("out of range")
+            result = 0
         finally:
             length.append(result)
         count += 1
