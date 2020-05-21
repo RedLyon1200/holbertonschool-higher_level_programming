f569654e181f102e3b24ff83344dbeed9b7e416e
xXRedLyonXx <s.agudelo1200@protonmail.com>
2020-05-21 04:06:46 +0000

    Task 3 | Test 0

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define class Square"""
+
+
+class Square:
+    """init to define size"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
