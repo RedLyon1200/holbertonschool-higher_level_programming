44e68c55c6468f121b0b177cb23b7c1cb27ceadd
xXRedLyonXx <s.agudelo1200@protonmail.com>
2020-05-23 03:49:26 +0000

    15 checks created, including import

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """
+    Function that prints My name is <first name> <last name>
+
+    Args:
+        first_name: first name :v
+        last_name: (optional) lastname
+    Raises:
+        TypeError:
+            first_name must be a string
+            last_name must be a string
+    """
+    # asdasdasdasd
+    # asdasdasdasd
+    # asdasdasdasd
+    # asdasdasdasd
+    # asdasdasdasd
+    # asdasdasdasd
+    # asdasdasdasd
+
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print('My name is {} {}'.format(first_name, last_name))
