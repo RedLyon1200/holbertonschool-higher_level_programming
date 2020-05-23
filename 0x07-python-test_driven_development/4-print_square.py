944a6c3deb9457704ffba5f05a46e97a48226b61
xXRedLyonXx <s.agudelo1200@protonmail.com>
2020-05-23 05:26:27 +0000

    Reoved float test for cases

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,14 +3,14 @@ def print_square(size):
     """[unction that prints a square with the character #]
 
     Arguments:
-        size {[type]} -- [the size length of the square]
+        size {[int]} -- [the size length of the square]
 
     Raises:
         TypeError: [size must be an integer]
         ValueError: [size must be >= 0]
     """
 
-    if type(size) is not int:
+    if type(size) != int:
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
