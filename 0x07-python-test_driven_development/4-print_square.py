ba2b9f16221cda2b45f1c68a6b91624f673d14b2
xXRedLyonXx <s.agudelo1200@protonmail.com>
2020-05-23 05:20:11 +0000

    Unnecessary conditional removed

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 def print_square(size):
-    """
-    function that prints a square with the character #
+    """[unction that prints a square with the character #]
 
+    Arguments:
+        size {[type]} -- [the size length of the square]
 
-    Args:
-        size: the size length of the square
     Raises:
-        TypeError:
-            size must be an integer
-        ValueError:
-            size must be >= 0
+        TypeError: [size must be an integer]
+        ValueError: [size must be >= 0]
     """
+
     if type(size) not in [int, float]:
-        raise TypeError('size must be an integer')
-    if type(size) is float and size < 0:
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
 
-    for pos in range(int(size)):
-        print('#' * int(size))
+    for pos in range(size):
+        print('#' * size)
