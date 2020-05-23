200a19cd017579b2217c4de99b00781f38ac7720
xXRedLyonXx <s.agudelo1200@protonmail.com>
2020-05-23 05:23:14 +0000

    removed type = float

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,7 +10,7 @@ def print_square(size):
         ValueError: [size must be >= 0]
     """
 
-    if type(size) not in [int, float]:
+    if type(size) is not int:
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
