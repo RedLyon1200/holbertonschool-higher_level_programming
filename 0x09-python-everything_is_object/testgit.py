1254770d1ef06ff9921b6f94ec144321ac7baf17
Crashed Code <s.agudelo1200@protonmail.com>
2020-05-27 05:55:20 +0000

    testgit.py added

---
+++ 0x09-python-everything_is_object/testgit.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import os
+
+print('\nComentando archivos modificados...')
+os.system('git status -s . | grep \' M \' > gitstatusM.txt')
+os.system("sed -i 's/\ M //g' gitstatusM.txt")
+
+f = open('gitstatusM.txt', 'r')
+while(True):
+    linea = f.readline()
+    os.system('git add {}'.format(linea))
+    print('Modificar valor del commit para {}?'.format(linea))
+    res = input('0 - No\n1 - Si\nOpc:')
+    if res is 1:
+        commit = input('Commit: ')
+    elif res == 0:
+        commit = 'file {} added'.format(linea)
+    else:
+        raise ValueError('Opcion inválida, debe ser 0 o 1')
+        os.system('git reset HEAD *')
+    os.system("git commit -m '{}'".format(commit))
+    if not linea:
+        break
+os.system('git push -u origin master')
