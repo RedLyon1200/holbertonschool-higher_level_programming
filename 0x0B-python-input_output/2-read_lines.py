20f919db00dffe9d915edaa1b2c5a2aaea793ef7
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-03 21:34:18 +0000

    Task: 2. Read n lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""[function that reads n lines of a text file (UTF8) and prints it to stdout:]
+    """
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    Keyword Arguments:
+        filename {str} -- [file path] (default: {""})
+        nb_lines {int} -- [number of lines to read] (default: {0 - all})
+    """
+    with open(filename, encoding='UTF-8') as f_obj:
+        if nb_lines <= 0:
+            print(f_obj.read())
+        else:
+            contents = f_obj.readlines()
+            print(''.join(contents[:nb_lines]), end='')
