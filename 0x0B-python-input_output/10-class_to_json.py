749b5ec847ca375f4131615fb8b2fe340e163264
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-03 21:35:35 +0000

    Task: 10. Class to JSON

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""[function that returns the dictionary description with simple
+    data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object]
+    """
+
+
+def class_to_json(obj):
+    """
+    Args:
+        obj ([instance]): [instance of a Class]
+
+    Returns:
+        [list, dictionary, string, integer and boolean]:
+        [the dictionary description with simple data structure]
+    """
+    return obj.__dict__
