5b35fa4bc2bc47b1ea79e77f8051a3748163821b
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-03 21:36:35 +0000

    Task: 12. Student to JSON with filter

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""[a class Student that defines a student (based on 12-student.py)]"""
+
+
+class Student:
+    """
+    """
+    pass
+
+    def __init__(self, first_name, last_name, age):
+        """[Instantiation with first_name, last_name and age]
+
+        Args:
+            first_name ([str])
+            last_name ([str])
+            age ([int])
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns:
+            [list, dictionary, string, integer and boolean]:
+            [the dictionary description with simple data structure]
+        """
+        return self.__dict__
