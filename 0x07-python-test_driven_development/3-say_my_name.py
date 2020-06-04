36b1d8151068b5565ad93060d006cd20bcfb0b44
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-04 00:46:49 +0000

    Task: 2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,17 +15,10 @@ def say_my_name(first_name, last_name=""):
             first_name must be a string
             last_name must be a string
     """
-    # asdasdasdasd
-    # asdasdasdasd
-    # asdasdasdasd
-    # asdasdasdasd
-    # asdasdasdasd
-    # asdasdasdasd
-    # asdasdasdasd
 
-    if type(first_name) != str:
+    if type(first_name) is not str:
         raise TypeError('first_name must be a string')
-    if type(last_name) != str:
+    if type(last_name) is not str:
         raise TypeError('last_name must be a string')
 
     print('My name is {:s} {:s}'.format(first_name, last_name))
