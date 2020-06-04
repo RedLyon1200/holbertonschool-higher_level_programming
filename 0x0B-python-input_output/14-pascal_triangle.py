6318f9a3dc24b17744a0b5ac529e38415e0fccb5
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-04 02:35:53 +0000

    A?

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+list of lists of integers representing the Pascal’s triangle
+
+
+def pascal_triangle(n):
+    """
+    Args:
+        n ([int]): [n times]
+
+    Returns:
+        [type]: [a list of lists of integers representing
+        the Pascal’s triangle of n:]
+    """
+    li = [[1]]
+    if n <= 0:
+        return []
+    for row in range(1, n):
+        li.append([1])
+        for count in range(row):
+            try:
+                num = li[row - 1][count + 1] + li[row - 1][count]
+            except Exception:
+                num = 1
+            if row - 1 == count:
+                num = 1
+            li[row].append(num)
+    return li
