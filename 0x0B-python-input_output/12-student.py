07d4c8fbc369a2836f76d79ee987f93153a6e16e
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-04 02:45:15 +0000

    Task: 12. Student to JSON with filter

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""[a class Student that defines a student (based on 12-student.py)]"""
+"""[a class Student that defines a student (based on 11-student.py)]"""
 
 
 class Student:
