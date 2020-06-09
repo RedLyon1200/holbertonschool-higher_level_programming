bfb639f9f43ae9b3cf94bf6f6c74d79004d4bee6
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-09 21:44:43 +0000

    Pep 8 modified

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -21,11 +21,13 @@ class TestSquare(unittest.TestCase):
         """ print('setUp') """
         Base._Base__nb_objects = 0
 
-    def test_style_pep8(self):
-        """[pep8]"""
+    def test_style_rectangle(self):
+        """
+        Tests for pep8
+        """
         style = pep8.StyleGuide(quiet=True)
-        result = style.check_files(['./models/square.py'])
-        self.assertEqual(result.total_errors, 0)
+        p = style.check_files(['models/square.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_sqr_id(self):
         """[OK]"""
