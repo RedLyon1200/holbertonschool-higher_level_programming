421fd25f8d89e16c459c3838ed1651fd2ce28e2e
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-09 19:37:38 +0000

    5-main.py added

--- 0x0C-python-almost_a_circle/5-main.py
+++ 0x0C-python-almost_a_circle/5-main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """ 5-main """
 from models.rectangle import Rectangle
+from models.square import Square
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(4, 6, 2, 1, 12)
+    r1 = Rectangle(4, 6, 2, 1)
     print(r1)
 
     r2 = Rectangle(5, 5, 1)
     print(r2)
 
+    s1 = Square(4)
+    print(s1)
+
+    s2 = Square(5)
+    print(s2)
