5da8062019b10beba75e74963cc026eb65a20d27
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-10 03:37:37 +0000

    bracket removed from comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""[the first class Base]"""
+"""the first class Base"""
 
 import json
 import csv
@@ -7,13 +7,13 @@ import os
 
 
 class Base:
-    """[manage id attribute in all your future classes
+    """manage id attribute in all your future classes
     and to avoid duplicating the same code]
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """[id is not None, assign the public instance attribute id
+        """id is not None, assign the public instance attribute id
 
         otherwise, increment __nb_objects and assign the
         new value to the public instance attribute id]
@@ -29,7 +29,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """[JSON is one of the standard formats for sharing
+        """JSON is one of the standard formats for sharing
             data representation.]
 
         Args:
@@ -44,7 +44,7 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """[writes the JSON string representation of list_objs to a file]"""
+        """writes the JSON string representation of list_objs to a file"""
         with open('{}.json'.format(cls.__name__), 'w') as f_obj:
             if list_objs is None:
                 f_obj.write(cls.to_json_string(None))
@@ -54,14 +54,14 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """[the list of the JSON string representation json_string]"""
+        """the list of the JSON string representation json_string"""
         if json_string is None or len(json_string) <= 0:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """[Create, update and return a new Rectangle or Square instance]
+        """Create, update and return a new Rectangle or Square instance]
 
         Returns:
             [instane]: [an instance with all attributes already set]
@@ -76,7 +76,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """[load information from a file]
+        """load information from a file]
 
         Returns:
             [list]: [list of instances]
