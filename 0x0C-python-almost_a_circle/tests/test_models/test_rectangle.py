34b11e6ba718375df1f797cfeabbeecf25efebbe
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-10 04:03:50 +0000

    Test pep 8 changed

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,13 +19,12 @@ class TestsRectangle(unittest.TestCase):
         """ print('setUp') """
         Base._Base__nb_objects = 0
 
-    def test_style_rectangle(self):
-        """
-        Tests for pep8
-        """
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/rectangle.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py'])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
 
     def test_rect_id(self):
         """OK"""
