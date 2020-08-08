ac7b793a3ede5dabfc2a1182839a64ad391153a8
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-08 00:39:34 +0000

    Task: 2. Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""[a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.]
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """[validate the quantity of arguments passed]
+    """
+    if len(argv) != 5:
+        print('Usage: 0-select_states user psw db state_name_searched')
+        exit(1)
+
+    host = 'localhost'
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    search = argv[4]
+
+    db = MySQLdb.connect(
+        host=host,
+        port=port,
+        user=user,
+        passwd=passwd,
+        db=db_name
+    )
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(search))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    db.close()
