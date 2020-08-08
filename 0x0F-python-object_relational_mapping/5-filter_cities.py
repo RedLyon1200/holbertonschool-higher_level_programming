7033f04c4b472e54217afd03278fabe427ff3e73
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-08 20:19:18 +0000

    5-filter_cities.py added.

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""[script that lists all cities from the database hbtn_0e_4_usa]
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """[validate the quantity of arguments passed]
+    """
+    if len(argv) != 5:
+        print('Usage: 4-cities_by_state user psw db search_state_name')
+        exit(1)
+
+    host = 'localhost'
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    search = argv[4]
+
+    db = MySQLdb.connect(
+        host=host,
+        port=port,
+        user=user,
+        passwd=passwd,
+        db=db_name
+    )
+    cursor = db.cursor()
+
+    cursor.execute("""
+        SELECT
+            cities.name
+        FROM
+            cities
+        WHERE
+            cities.state_id = (
+                SELECT
+                    id
+                FROM
+                    states
+                WHERE
+                    name = %s
+            )
+        ORDER BY
+            cities.id ASC
+        """, (search,))
+
+    cities = cursor.fetchall()
+    for city in cities:
+        print(city)
+    cursor.close()
+    db.close()
