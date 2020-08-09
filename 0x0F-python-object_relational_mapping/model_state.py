11332cad85110d0cd1e86e4e2937ac21a1c83219
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-09 14:29:43 +0000

    attributes declared in a unique line

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,6 @@
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -19,14 +18,6 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(
-        Integer,
-        primary_key=True,
-        nullable=False,
-        autoincrement=True,
-        unique=True
-    )
-    name = Column(
-        String(128),
-        nullable=False
-    )
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
