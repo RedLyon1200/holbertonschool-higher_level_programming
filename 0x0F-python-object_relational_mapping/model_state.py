d2957060ea61d33f88b22d2f0c23e924cc3a0c32
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-09 14:31:40 +0000

    comments at the line 2 chaged

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# contains the class definition of a State and an instance
-# Base = declarative_base()
+"""[contains the class definition of a State and an instance
+Base = declarative_base()]
+"""
 
 from sqlalchemy import Column
 from sqlalchemy import Integer
@@ -18,6 +19,14 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
-    name = Column(String(128), nullable=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True,
+        unique=True
+    )
+    name = Column(
+        String(128),
+        nullable=False
+    )
