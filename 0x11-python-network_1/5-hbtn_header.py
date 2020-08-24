6c1710dbad1c0d44c5fbfa3243ec64cf2fe54be9
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 20:23:44 +0000

    Task: 5. Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""[summary]
+"""
+
+import requests
+
+
+def fetches(url):
+    """[summary]
+    """
+    response = requests.get(url)
+
+    print('{}'.format(response.headers['X-Request-Id']))
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+
+    fetches(url)
