fe10c22ede20b1f704b449e40e42b33d517d87dd
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 20:38:47 +0000

    Task: 6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""[summary]
+"""
+
+import requests
+from sys import argv
+
+
+def fetches(url, mail):
+    """[summary]
+    """
+    url_params = {'email', mail}
+    response = requests.post(url, data=url_params)
+
+    print('{}'.format(response.text))
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    mail = argv[2]
+
+    fetches(url, mail)
