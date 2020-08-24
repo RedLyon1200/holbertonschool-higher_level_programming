d0d33a9bc06d2fbe1f92c5291e4fbb9376430ba0
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 20:17:27 +0000

    Task: 4. What´s my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,13 +6,13 @@ import requests
 
 
 def fetches(url):
-    """
+    """[summary]
     """
     response = requests.get(url)
 
     if response.status_code == 200:
         print('Body response:')
-        print('\t- type {}'.format(type(response)))
+        print('\t- type {}'.format(type(response.text)))
         print('\t- content: {}'.format(response.text))
 
 
