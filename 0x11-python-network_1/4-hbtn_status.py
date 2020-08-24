2fa02eba4e07ad5169f1237dcbf5e14772bed594
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 20:10:58 +0000

    Task: 4. What´s my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""[summary]
+"""
+
+import requests
+
+
+def fetches(url):
+    """
+    """
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print('Body response:')
+        print('\t- type {}'.format(type(response.text)))
+        print('\t- content: {}'.format(response.text))
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+
+    fetches(url)
