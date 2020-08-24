75f46e74e46b1bf698d4fb021b7865bd48c1cc76
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 19:50:25 +0000

    HHTPError used

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,10 +2,10 @@
 """
 script that takes in a URL, sends a request to the URL and
 displays the value of the X-Request-Id variable found in
-the header of the response.
+the header of the response
 """
 from urllib.request import Request, urlopen
-from urllib.error import URLError
+from urllib.error import HTTPError
 from sys import argv
 
 
@@ -18,9 +18,8 @@ def fetche(url):
     try:
         response = urlopen(url).read()
         print(response.decode('utf-8'))
-    except URLError as e:
-        if hasattr(e, 'code'):
-            print('Error code: {}'.format(e.code))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
 
 
 if __name__ == "__main__":
