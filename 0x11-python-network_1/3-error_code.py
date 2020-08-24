f627d0258cf68c61d88bb84d35b3bb869b5139b0
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 19:57:51 +0000

    with used

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,7 +4,7 @@ script that takes in a URL, sends a request to the URL and
 displays the value of the X-Request-Id variable found in
 the header of the response
 """
-from urllib.request import Request, urlopen
+import urllib.request
 from urllib.error import HTTPError
 from sys import argv
 
@@ -13,11 +13,13 @@ def fetche(url):
     """
     fetches X-Request-Id value in a url
     """
-    req = Request(url)
+    pass
 
     try:
-        response = urlopen(url).read()
-        print(response.decode('utf-8'))
+        with urllib.request.urlopen(url) as response:
+            html_content = response.read()
+
+            print(html_content.decode('utf-8'))
     except HTTPError as e:
         print('Error code: {}'.format(e.code))
 
