12b6fc4223f11563d2d85aa343920b38f57da798
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 21:54:25 +0000

    Task: 9. My Github!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""[summary]
+"""
+
+import requests
+from sys import argv
+
+
+def fetches(user, pwd):
+    """[summary]
+    """
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(user, pwd))
+    print(response.json().get('id'))
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    pwd = argv[2]
+
+    fetches(user, pwd)
