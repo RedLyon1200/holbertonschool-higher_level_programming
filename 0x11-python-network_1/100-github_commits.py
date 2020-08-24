b0b2166dd0bcc5691e5e1f519dd47c82db9cf444
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 22:13:52 +0000

    Task: 10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""[summary]
+"""
+
+import requests
+from sys import argv
+
+
+def fetches(repo, user):
+    """[summary]
+    """
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        user, repo
+    )
+    response = requests.get(url)
+    json_obj = response.json()
+    for item in json_obj[:10]:
+        sha = item.get('sha')
+        item = item.get('commit')
+        print('{}: {}'.format(sha, item.get('author').get('name')))
+
+
+if __name__ == "__main__":
+    repo = argv[1]
+    user = argv[2]
+
+    fetches(repo, user)
