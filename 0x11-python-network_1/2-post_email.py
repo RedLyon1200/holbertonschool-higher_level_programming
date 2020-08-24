63bbdc7e6ff005ce7172e5ee9c635d0b69ae3ca5
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 18:04:08 +0000

    Task: 2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
+import urllib.request
+import sys
+
+
+def fetche(url, email):
+    """
+    fetches X-Request-Id value in a url
+    """
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+
+    with urllib.request.urlopen(url, data) as response:
+        html_content = response.read()
+
+        print(html_content.decode('utf-8'))
+
+
+if __name__ == "__main__":
+    """entry point
+    """
+    url = sys.argv[1]
+    mail = sys.argv[2]
+
+    fetche(url, mail)
