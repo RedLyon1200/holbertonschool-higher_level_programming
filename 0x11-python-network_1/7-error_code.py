a133a4378f5a2b433ed212a3458d76a260c91d0e
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-24 20:43:38 +0000

    Task: 7. Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""[summary]
+"""
+
+import requests
+
+
+def fetches(url):
+    """[summary]
+    """
+    response = requests.get(url)
+    status_code = response.status_code
+
+    if status_code >= 400:
+        print('Error code: {}'.format(status_code))
+    else:
+        print(response.text)
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+
+    fetches(url)
