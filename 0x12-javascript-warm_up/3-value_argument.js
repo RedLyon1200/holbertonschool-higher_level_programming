9c98c2f1d62343020198535a8762527137a5c163
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 22:17:15 +0000

    const added

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // a script that prints the first argument passed to it:
-argv1 = process.argv[2];
+const argv1 = process.argv[2];
 argv1 ? console.log(argv1) : console.log('No argument');
