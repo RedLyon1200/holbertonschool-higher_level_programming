cabacf6a45ab133fed4cebb229983db9dbbafe61
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 22:39:50 +0000

    change let to const

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-let array = ['C is fun', 'Python is cool', 'Javascript is amazing']
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing']
 
 for (i = 0; i < array.length; i++) {
     console.log(array[i]);
