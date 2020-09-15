b9f4c442f3341ac7d6b6955479c94d739e1298d4
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 22:42:36 +0000

    dot comma added

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints 3 lines: (like 1-multi_languages.js)
 // but by using an array of string and a loop
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing']
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
