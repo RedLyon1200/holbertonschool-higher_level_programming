76114d8e98f7bfed2867912394e1bc04edb9aa65
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 22:45:32 +0000

    1 shor comment line

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// script that prints 3 lines: (like 1-multi_languages.js)
-// but by using an array of string and a loop
+// script that prints 3 lines using an array of string and a loop
 const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < myArray.length; i++) {
