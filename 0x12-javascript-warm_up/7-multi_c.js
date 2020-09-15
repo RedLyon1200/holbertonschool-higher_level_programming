ded8259977b29c3193d88f8786e8e83d0c67f4fe
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 22:55:57 +0000

    7-multi_c.js added

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints x times “C is fun”
+let num = !isNaN(parseInt(process.argv[2])) ? parseInt(process.argv[2]) : 'Missing number of occurrences';
+
+if (num === 'Missing number of occurrences') {
+    console.log(num);
+    return;
+}
+
+while (num--) {console.log('C is fun');}
