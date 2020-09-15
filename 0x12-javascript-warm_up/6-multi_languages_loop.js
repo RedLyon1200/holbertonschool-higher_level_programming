bbfdd917bb0b7991a095c62b56a892edbb080e57
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 22:41:36 +0000

    declare var let into for statement

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-// script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+// script that prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing']
 
-for (i = 0; i < array.length; i++) {
+for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
 }
