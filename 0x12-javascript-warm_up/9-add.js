26a28ee16558f91a51d380038d40dd2b1ab8ff73
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 23:29:54 +0000

    9-add.js added

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints the addition of 2 integers
+const num1 = !isNaN(parseInt(process.argv[2])) ? parseInt(process.argv[2]) : false;
+const num2 = !isNaN(parseInt(process.argv[2])) ? parseInt(process.argv[3]) : false;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+num1 && num2 ? console.log(add(num1, num2)) : console.log(NaN);
