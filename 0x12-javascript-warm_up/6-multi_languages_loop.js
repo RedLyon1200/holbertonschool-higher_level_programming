849849564870c4caa9baea96c354573d199e40ad
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 22:44:19 +0000

    camelCase

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // script that prints 3 lines: (like 1-multi_languages.js)
 // but by using an array of string and a loop
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
 }
