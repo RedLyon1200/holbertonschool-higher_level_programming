0391e3528ae22009aea09fd44183c69b8e673d1d
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-16 01:23:45 +0000

    array declared in one line

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 // script that prints 3 lines using an array of string and a loop
-const myArray = [
-  'C is fun',
-  'Python is cool',
-  'Javascript is amazing'
-];
+
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
