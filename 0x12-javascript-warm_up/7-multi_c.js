6b2b41fb3b53a002d6cc7e718544bf3a247fc7c8
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-16 01:31:05 +0000

    semistandar fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // script that prints x times “C is fun”
-let num = !isNaN(parseInt(process.argv[2])) ? parseInt(process.argv[2]) : 'Missing number of occurrences';
 
-if (num === 'Missing number of occurrences') {
-    console.log(num);
-    return;
+let num = !isNaN(parseInt(process.argv[2])) ? parseInt(process.argv[2]) : false;
+
+if (!num) {
+  console.log('Missing number of occurrences');
+} else {
+  while (num-- && num >= 0) { console.log('C is fun'); }
 }
-
-while (num-- && num >= 0) {console.log('C is fun');}
