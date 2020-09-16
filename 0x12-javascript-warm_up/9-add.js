b1749e4425d72d50c2f0277582389d25bc404398
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-16 01:32:36 +0000

    semistandar fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,8 +4,8 @@
 const num1 = !isNaN(parseInt(process.argv[2])) ? parseInt(process.argv[2]) : false;
 const num2 = !isNaN(parseInt(process.argv[2])) ? parseInt(process.argv[3]) : false;
 
-function add(num1, num2) {
-    return num1 + num2;
+function add (num1, num2) {
+  return num1 + num2;
 }
 
 num1 && num2 ? console.log(add(num1, num2)) : console.log(NaN);
