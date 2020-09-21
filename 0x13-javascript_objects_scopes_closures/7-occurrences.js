a652069c4d51c959e1c388e6005bbb72b15e6912
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-21 19:45:02 +0000

    7. Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+exports.nbOccurences = function (list, searchElement) {
+  // returns the number of occurrences in a list
+  let count = 0;
+
+  for (let index = 0; index < list.length; index++) {
+    if (list[index] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
