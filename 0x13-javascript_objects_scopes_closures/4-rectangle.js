398ed0edcfb5a64c950518f74da36116539e01ca
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-21 19:08:57 +0000

    Task: 4. Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+// -*- coding utf-8 -*-
+class Rectangle {
+  // Constructor
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // prints the rectangle using the character X
+  print() {
+    for (let y = 0; y < this.height; y++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  // exchanges the width and the height of the rectangle
+  rotate() {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  // multiples the width and the height of the rectangle by 2
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
