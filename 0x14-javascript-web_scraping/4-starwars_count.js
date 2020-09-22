eafa765e40567f95f2998f94a7cb87db896495e6
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-22 20:38:36 +0000

    Task: 4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,22 @@
 #!/usr/bin/node
 // -*- coding utf-8 -*-
 const request = require('request');
-const id = 18; // Wedge Antilles
-const url = 'https://swapi-api.hbtn.io/api/people/' + id;
+const url = process.argv[2];
+const user = 18;
+let nb = 0;
 
 request(url, function (err, res, body) {
   if (err) {
     console.log(err);
-  } else {
-    console.log(JSON.parse(body).films.length);
+  } else if (res.statusCode === 200) {
+    body = JSON.parse(body).results;
+    for (const film of body) {
+      for (const c of film.characters) {
+        if (c.includes(user)) {
+          nb += 1;
+        }
+      }
+    }
+    console.log(nb);
   }
 });
