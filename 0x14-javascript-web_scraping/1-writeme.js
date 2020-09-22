8d0af84de9ff3f0c9d7ce7c215d05113da4fdd91
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-22 20:01:25 +0000

    Task: 1. Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// -*- coding utf-8 -*-
+const fs = require('fs');
+const file = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(file, data, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
